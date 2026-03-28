a2b6eef4d1ca9079fd5a15d3007f73df4399e3ff
zippity201 <evgeniy.savchenko79@gmail.com>
2026-03-28 13:51:25 +0700

    refactor: компиляция в файл без внешних библиотек

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define BUILD_NUMBER 11
+#define BUILD_NUMBER 13
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
 
-#define VERSION_STRING "0.1.11"
+#define VERSION_STRING "0.1.13"
 
 #endif // VERSION_H
