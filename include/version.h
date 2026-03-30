07122bc1c07038b33460a25b4067b796817dc8ca
zippity201 <evgeniy.savchenko79@gmail.com>
2026-03-30 21:14:54 +0700

    refactor: версия проекта

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define BUILD_NUMBER 24
+#define BUILD_NUMBER 25
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
 
-#define VERSION_STRING "0.1.24"
+#define VERSION_STRING "0.1.25"
 
 #endif // VERSION_H
