34a24a200874f82b9915b08480dc361bbaf1267e
zippity201 <evgeniy.savchenko79@gmail.com>
2026-03-30 19:26:06 +0700

    refactor: рефакторинг кода

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define BUILD_NUMBER 22
+#define BUILD_NUMBER 23
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
 
-#define VERSION_STRING "0.1.22"
+#define VERSION_STRING "0.1.23"
 
 #endif // VERSION_H
